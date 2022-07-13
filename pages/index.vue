3b3ae85ac3b13f0d214e3b28a24010a8d16701ea
Petr <petr@iMac-Petr.local>
2022-07-13 15:10:27 +0300

    first

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <header class="header">
+      <div class="item">
+        <div>раз</div>
+        <div>два</div>
+      </div>
+    </header>
+    <div class="content">
+      <div class="slides">
+        <img src="../assets/img/test.png" alt="" />
+      </div>
+    </div>
+    <div class="footer">
+      <div class="item">
+        <div>контакты</div>
+        <div>к проекту</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
